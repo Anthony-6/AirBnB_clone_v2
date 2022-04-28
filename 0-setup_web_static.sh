d7fcf3a4694cc041a3841aa256e264bb9c735d29
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-28 15:49:34 +0200

    add file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# comment
-apt-get -y update
-apt-get -y install nginx
+# script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/webstatic/releases/test/
 mkdir -p /data/webstatic/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ls -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 sed -i 'location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx restart
