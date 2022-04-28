0fd000e04758849c125ff843a62262789d20dc12
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-28 22:37:44 +0200

    update file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
+apt-get -y update
+apt-get -y install nginx
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "test conf" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-sudo service nginx restart
+sed -i '/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
