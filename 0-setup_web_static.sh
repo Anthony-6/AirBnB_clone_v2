489a210fc6c5d4bcd96fb57ada4fbbaad7daeb34
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-28 17:22:28 +0200

    update file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,15 @@ apt-get -y install nginx
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Simple content to test nginx configuration" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+echo "<html>
+  <head>
+  </head>
+  <body>
+    test conf
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
+exit 0
