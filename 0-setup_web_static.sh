f36ed3eacf4db562a895eb29c2799d69e574c891
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-28 19:33:20 +0200

    update file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,6 @@ mkdir -p /data/web_static/shared/
 echo "test conf" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a\t location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
