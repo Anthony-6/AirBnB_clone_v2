e8f692a5ae7500e82af8f603a4fbedffb0ceea8a
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-29 15:31:40 +0200

    fix file and exit code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,6 @@ mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '\t location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sudo sed -i '\t location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 service nginx restart
+exit 0
