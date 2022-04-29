48ff71890d2b61bf87803be8f20cc261f2202790
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-29 16:04:08 +0200

    update file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sudo sed -i '\t location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
