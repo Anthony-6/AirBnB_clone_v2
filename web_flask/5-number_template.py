489b4bef0d0b17a8cca4f68fdbf90aeb947bdfcf
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-14 13:29:06 +0200

    update file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -45,7 +45,6 @@ def fifth(n):
 def sixth(n):
     ''' display HTML page and create balise h1 using N as parameter '''
     if isinstance(n, int) is True:
-        newN = str(n)
         text = f'Number: {n}'
         return render_template('5-number.html', text=text)
 
